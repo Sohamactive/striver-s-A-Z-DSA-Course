163f686521b8feb2719270d62fc8ffce9825af96
sohamactive <sharmasoham1427@gmail.com>
2025-11-03 12:43:44 +0530

    step2.1 updated,started step2.2

--- step_2/lecture_2/merge_sort.cpp
+++ step_2/lecture_2/merge_sort.cpp
@@ -1,2 +1,49 @@
-//O(nlog(n))
-//divide and merge
+#include<bits/stdc++.h>
+using namespace std;
+
+void merge(vector<int> &arr,int low,int mid , int high){
+        vector<int> temp;
+        int left = low;
+        int right = mid+1;
+        while(left<=mid && right <=high){
+            if(arr[left]<=arr[right]){
+                temp.push_back(arr[left]);
+                left++;
+            }
+            else {temp.push_back(arr[right]);right++;}
+
+        }
+        while(left<=mid){
+            temp.push_back(arr[left]);
+            left++;
+        }
+        
+        while(right<=high){
+            temp.push_back(arr[right]);
+            right++;
+        }
+        
+        for(int i=low;i<=high;i++){
+            arr[i]=temp[i-low];
+        }
+        
+    }
+    void ms(vector<int> &nums, int low, int high){
+        if(low>=high) return ;
+        int mid = low + (high-low)/2;
+        ms(nums,low,mid);
+        ms(nums,mid+1,high);
+        merge(nums,low,mid,high);
+    }
+    vector<int> mergeSort(vector<int>& nums) {
+        ms(nums,0,nums.size()-1);
+        return nums;
+    }
+    int main(){
+        vector<int> a={7,4,1,5,3};
+        mergeSort(a);
+        for(auto i : a){
+            cout << i << ' ';
+        }
+
+    }
